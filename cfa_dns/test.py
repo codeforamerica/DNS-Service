ed49712e7df5aef43214027c2bfa47aab5a61da4
Michal Migurski <mike@teczno.com>
2015-04-22 16:26:41 -0700

    Added tests for api.check_upstream() and CSV hash consistency

--- cfa_dns/test.py
+++ cfa_dns/test.py
@@ -1,7 +1,13 @@
 from os.path import dirname, join
+from urlparse import urlparse, parse_qsl
+from csv import DictReader
+from io import StringIO
 import unittest
 
+from httmock import response, HTTMock
+
 from . import check_file
+from .api import hash_host_records, format_csv_row, check_upstream
 
 class TestFile (unittest.TestCase):
 
@@ -9,5 +15,104 @@ class TestFile (unittest.TestCase):
         filename = join(dirname(__file__), '..', 'host-records.csv')
         check_file(filename)
 
+class TestAPI (unittest.TestCase):
+
+    api_error_xml = '''<?xml version="1.0" encoding="utf-8"?>
+        <ApiResponse Status="ERROR" xmlns="http://api.namecheap.com/xml.response">
+          <Errors>
+            <Error Number="999">{}</Error>
+          </Errors>
+          <Warnings />
+          <RequestedCommand />
+          <Server>PHX01APIEXT01</Server>
+          <GMTTimeDifference>--4:00</GMTTimeDifference>
+          <ExecutionTime>0</ExecutionTime>
+        </ApiResponse>
+        '''
+    
+    STEP_BAD_HASH = 1
+
+    def setUp(self):
+        '''
+        '''
+        self.api_base = 'http://example.com/root'
+        _, self.api_host, self.api_path, _, _, _ = urlparse(self.api_base)
+    
+    def response_content(self, step=None):
+      '''
+      '''
+      case = self
+      
+      def thing_doer(url, request):
+        scheme, host, path, _, query, _ = urlparse(url.geturl())
+        headers = {'Content-Type': 'text/xml; charset=utf-8'}
+        
+        if (host, path) == (case.api_host, case.api_path):
+            args = dict(parse_qsl(query))
+            
+            if (request.method, args['Command']) == ('GET', 'namecheap.domains.dns.getHosts'):
+                body = '''<?xml version="1.0" encoding="utf-8"?>
+                    <ApiResponse Status="OK" xmlns="http://api.namecheap.com/xml.response">
+                      <Errors />
+                      <Warnings />
+                      <RequestedCommand>namecheap.domains.dns.getHosts</RequestedCommand>
+                      <CommandResponse Type="namecheap.domains.dns.getHosts">
+                        <DomainDNSGetHostsResult Domain="codeforamerica.org" EmailType="MX" IsUsingOurDNS="true">
+                          <host HostId="69617219" Name="*.s" Type="A" Address="23.21.64.76" MXPref="0" TTL="300" AssociatedAppTitle="" FriendlyName="" IsActive="" />
+                          <host HostId="69617220" Name="@" Type="A" Address="54.234.33.69" MXPref="0" TTL="60" AssociatedAppTitle="" FriendlyName="" IsActive="" />
+                          <host HostId="69617246" Name="boston" Type="A" Address="66.220.0.85" MXPref="0" TTL="1800" AssociatedAppTitle="" FriendlyName="" IsActive="" />
+                          <host HostId="69617319" Name="www" Type="CNAME" Address="ec2-54-234-33-69.compute-1.amazonaws.com." MXPref="0" TTL="60" AssociatedAppTitle="" FriendlyName="" IsActive="" />
+                          <host HostId="69617223" Name="@" Type="MX" Address="ASPMX.L.GOOGLE.COM." MXPref="10" TTL="300" AssociatedAppTitle="" FriendlyName="" IsActive="" />
+                          <host HostId="69739050" Name="hosts-hash" Type="TXT" Address="{hosts_hash}" MXPref="0" TTL="300" AssociatedAppTitle="" FriendlyName="" IsActive="" />
+                          <host HostId="69617283" Name="network" Type="URL301" Address="http://peernetwork.in" MXPref="0" TTL="1800" AssociatedAppTitle="" FriendlyName="" IsActive="" />
+                          {additional_host}
+                        </DomainDNSGetHostsResult>
+                      </CommandResponse>
+                      <Server>PHX01APIEXT01</Server>
+                      <GMTTimeDifference>--4:00</GMTTimeDifference>
+                      <ExecutionTime>0.013</ExecutionTime>
+                    </ApiResponse>
+                    '''
+            
+                vars = dict(additional_host='', hosts_hash='20bea02f8bf1fe5f9565e24b37353162ef5c73e9')
+                
+                if step == case.STEP_BAD_HASH:
+                    vars['hosts_hash'] = 'xyzygy'
+
+                return response(200, body.format(**vars), headers=headers)
+            
+            raise NotImplementedError(request.method, args)
+        
+        raise NotImplementedError(url.geturl())
+    
+      return thing_doer
+    
+    def test_check_upstream(self):
+        '''
+        '''
+        with HTTMock(self.response_content()):
+            check_upstream(self.api_base, '0xDEADBEEF')
+    
+        with self.assertRaises(ValueError):
+            with HTTMock(self.response_content(self.STEP_BAD_HASH)):
+                check_upstream(self.api_base, '0xDEADBEEF')
+    
+    def test_hash_consistency(self):
+        '''
+        '''
+        data = u'''Type,Host,Value,TTL,MXPref,Note
+A,*.s,23.21.64.76,300,,
+CNAME,www,ec2-54-234-33-69.compute-1.amazonaws.com.,60,,The website
+MX,@,ASPMX.L.GOOGLE.COM.,300,10,GMail
+A,@,54.234.33.69,60,,The website
+A,boston,66.220.0.85,1800,,
+URL301,network,http://peernetwork.in,1800,,
+'''
+        
+        host_records = list(DictReader(StringIO(data)))
+        hash = hash_host_records(map(format_csv_row, host_records))
+        
+        self.assertEqual(hash, '20bea02f8bf1fe5f9565e24b37353162ef5c73e9')
+        
 if __name__ == '__main__':
     unittest.main()

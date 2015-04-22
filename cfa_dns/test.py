b5ba1c8a7c74c2393bcd2288d4e80c9a2e0a07c7
Michal Migurski <mike@teczno.com>
2015-04-22 15:45:35 -0700

    Started using unittest module in tests

--- cfa_dns/test.py
+++ cfa_dns/test.py
@@ -1,6 +1,13 @@
-from sys import argv
+from os.path import dirname, join
+import unittest
+
 from . import check_file
-    
+
+class TestFile (unittest.TestCase):
+
+    def test_file(self):
+        filename = join(dirname(__file__), '..', 'host-records.csv')
+        check_file(filename)
+
 if __name__ == '__main__':
-    _, filename = argv
-    check_file(filename)
+    unittest.main()
